545b71edfc477130c99e9ad928fc484297be0400
omer-vishlitzky <omer.vishlitzky@gmail.com>
2026-09-16 10:42:47 +0300

    update comment

--- fulfillment-service/internal/servers/private_projects_server.go
+++ fulfillment-service/internal/servers/private_projects_server.go
@@ -180,9 +180,7 @@ func (s *PrivateProjectsServer) Create(ctx context.Context,
 		return
 	}
 
-	// When a project is created, the 'metadata.name' field must be the full name of the project, and the
-	// 'metadata.project' field must be the name of the parent project. If the parent project is not specified
-	// then it will be calculated from the the name, removing the last component.
+	// metadata.project must remain empty to derive the parent project from the path
 	var project string
 	if count > 1 {
 		project = strings.Join(path[:count-1], ".")
